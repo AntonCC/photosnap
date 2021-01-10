0f45bd6c26b76575d664a92806dd5d5f9ed0b49b
Anton <antonchetvv@gmail.com>
2021-01-10 12:49:46 -0500

    Home page made responsive for tablet viewport.

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,9 +3,12 @@ import './card.scss'
 import Button from '../button/button'
 // import Test from '../../assets/stories/desktop/calm-waters.jpg'
 
+
 const Card = ({ title, author, img }) => {
+  const backgroundImg = `linear-gradient(to bottom, rgba(0,0,0,0) 20%, rgba(0,0,0,1)), url(${img}) no-repeat center/cover`
+
   return (
-    <div className='card' style={{ background: `url(${img}) no-repeat center/cover` }}>
+    <div className='card' style={{ background: backgroundImg }}>
       <h3>{ title }</h3>
       <p>by { author }</p>
       <hr/>
